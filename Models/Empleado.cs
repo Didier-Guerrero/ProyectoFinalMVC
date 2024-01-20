33a4276ea29a1fe25f40d794b04b79fca8d51308
Didier Guerrero <didier.guerrero@udla.ede.ec>
2024-01-19 19:24:51 -0500

    Agregar archivos de proyecto.

---
+++ Models/Empleado.cs
@@ -0,0 +1,15 @@
+﻿using System.Net.Mail;
+
+namespace ProyectoFinal.Models
+{
+    public class Empleado
+    {
+        public int EmpleadoID { get; set; }
+        public string? NombreEmpleado { get; set; }
+        public string? CargoEmpleado { get; set; }    
+        public int EdadEmpleado { get; set; } 
+        public MailAddress? CorreoEmpleado { get; set; }
+
+
+    }
+}
